98a7e2a118614d2e45ee3bd373fa4b2c6e2ca883
bahaaltan <98954059+AltanReisoglu@users.noreply.github.com>
2025-09-26 22:07:16 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,26 @@
-import axios from 'axios';
+// api.js
+import axios from "axios";
+import { API_BASE_URL } from "./util";
 
-// Create an instance of axios with the base URL
+// axios instance: baseURL olarak API_BASE_URL kullan
 const api = axios.create({
-  baseURL: "/choreo-apis/myuni-tn/backend/v1"
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Export the Axios instance
+// request interceptor: localStorage'dan token alır ve header ekler
+api.interceptors.request.use((config) => {
+  try {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch (e) {
+    // ignore
+  }
+  return config;
+}, (error) => Promise.reject(error));
 
 export default api;
-
