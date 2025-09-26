9879b7391f34613cb3307fb9239f6d00b62777d4
bahaaltan <98954059+AltanReisoglu@users.noreply.github.com>
2025-09-26 22:19:10 +0300

    Update util.js

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,1 +1,7 @@
-export const API_BASE_URL = "/choreo-apis/myuni-tn/backend/v1/api"
+window.configs = {
+    apiUrl: '/choreo-apis/myuni-tn/backend/v1',
+};
+
+const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
+
+export const API_BASE_URL = apiUrl
