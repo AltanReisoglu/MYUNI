85b77fb5c8566bacd09c9750de70b639bee72756
bahaaltan <98954059+AltanReisoglu@users.noreply.github.com>
2025-09-26 20:53:41 +0300

    Update LoginScreen.jsx

--- frontend/src/LoginScreen.jsx
+++ frontend/src/LoginScreen.jsx
@@ -32,7 +32,7 @@ const LoginScreen = ({ onLogin }) => {
 
     setLoading(true);
     try {
-      const response =await api.post("/assistant/preset", {
+      const response =await api.post("/choreo-apis/myuni-tn/backend/v1/assistant/preset", {
         email:email,
         school:schoolName,
       });
@@ -118,3 +118,4 @@ const LoginScreen = ({ onLogin }) => {
 };
 
 export default LoginScreen;
+
