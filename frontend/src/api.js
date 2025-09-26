187ae533aa6935f64096651d5c68485e7b5b9d5b
bahaaltan <98954059+AltanReisoglu@users.noreply.github.com>
2025-09-26 20:54:08 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // Create an instance of axios with the base URL
 const api = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "/choreo-apis/myuni-tn/backend/v1"
 });
 
 // Export the Axios instance
+
 export default api;
