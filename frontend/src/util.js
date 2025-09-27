1bf1b15fe43339b64c2d843aa48d9fd4ddbe4a80
bahaaltan <98954059+AltanReisoglu@users.noreply.github.com>
2025-09-27 10:18:13 +0300

    Update util.js

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,5 +1,5 @@
 window.configs = {
-    apiUrl: '/choreo-apis/myuni-tn/backend/v1',
+    apiUrl: 'https://acf87dfe-494a-450d-830c-fb0b0ad24134-prod.e1-us-east-azure.choreoapis.dev/myuni-tn/backend/v1',
 };
 
 const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
