d784a3007e4f1486cc2ff4a581544118c53c5439
bahaaltan <98954059+AltanReisoglu@users.noreply.github.com>
2025-09-27 14:46:44 +0300

    Update util.js

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,6 +1,6 @@
 window.configs = {
-    apiUrl: 'https://acf87dfe-494a-450d-830c-fb0b0ad24134-dev.e1-us-east-azure.choreoapis.dev/myuni-tn/backend/v1'
+    apiUrl: '/choreo-apis/myuni-tn/backend/v1'
 };
+const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
 
-// MUTLAKA bu şekilde export edin
-export const API_BASE_URL = window.configs.apiUrl;
+export const API_BASE_URL = apiUrl;
