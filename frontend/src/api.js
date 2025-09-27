5c0175925848f4ef6945f90b0caad3b6d4d3c908
bahaaltan <98954059+AltanReisoglu@users.noreply.github.com>
2025-09-27 11:39:42 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,22 +5,8 @@ import { API_BASE_URL } from "./util";
 // axios instance: baseURL olarak API_BASE_URL kullan
 const api = axios.create({
   baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  timeout: 60000,
 });
 
-// request interceptor: localStorage'dan token alır ve header ekler
-api.interceptors.request.use((config) => {
-  try {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  } catch (e) {
-    // ignore
-  }
-  return config;
-}, (error) => Promise.reject(error));
 
-export default api;
+
